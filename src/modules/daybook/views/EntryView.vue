c550cce28c684ec3823c3b5efc8baef6b142ec13
Cmunoz12 <carlos.munoz@decathlon.com>
2022-04-27 16:45:16 +0200

    Seccion 11 terminada

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,41 +1,42 @@
 <template>
-    
-  <div  v-if="entry"
-        class="entry-title d-flex justify-content-betwen p-2">
-        <div >
-            <span class="text-success fs-3 fw-bold">{{day}}</span>
-            <span class="mx-1 fs-3">{{month}}</span>
-            <span class="mx-2 fs-4 fw-light">{{yearDay}}</span>
-        </div>
-        <div>
-            <button class="btn btn-danger mx-2">
-                borrar
-                <i class="fa fa-trash-alt"></i>
-            </button>
-            <button class="btn btn-primary">
-                subir foto
-                <i class="fa fa-upload"></i>
-            </button>
-        </div>
-    </div>
 
-    <hr>
-    <div 
-        v-if="entry"
-        class="d-flex flex-colum px-3 h-75">
-        <textarea
-            v-model="entry.text"
-            placeholder="¿Que sucedio hoy?"></textarea>
-
-    </div> 
+    <template v-if="entry">
+        <div class="entry-title d-flex justify-content-betwen p-2">
+            <div >
+                <span class="text-success fs-3 fw-bold">{{day}}</span>
+                <span class="mx-1 fs-3">{{month}}</span>
+                <span class="mx-2 fs-4 fw-light">{{yearDay}}</span>
+            </div>
+            <div>
+                <button class="btn btn-danger mx-2">
+                    borrar
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+                <button class="btn btn-primary">
+                    subir foto
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
+        </div>
+        
+        <hr>
+        
+        <div 
+             class="d-flex flex-colum px-3 h-75">
+            <textarea v-model="entry.text" 
+                      placeholder="¿Que sucedio hoy?">
+            </textarea>
+        </div>  
+        
+        <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/04/One-Piece-libera-un-historico-y-viral-episodio-1015.jpg?fit=1280%2C720&quality=80&ssl=1" 
+            alt="entry one piece picture"
+            class="img-thumbnail">
+    </template>
 
     <FabVue 
         icon="fa-save"
     />
     
-    <img src="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/04/One-Piece-libera-un-historico-y-viral-episodio-1015.jpg?fit=1280%2C720&quality=80&ssl=1" 
-         alt="entry one piece picture"
-         class="img-thumbnail">
 </template>
 
 <script>
